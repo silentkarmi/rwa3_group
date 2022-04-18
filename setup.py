2b1176da28768b632164fc46d439a532e941abbe
Kartikeya Mishra <kmishra@umd.edu>
2022-04-18 15:38:23 -0400

    First Aruco Marker getting detected

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python
 
-# DO NOT USE
-# python setup.py install
+# DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
 
 from distutils.core import setup
-# from setuptools import find_packages
-from catkin_pkg.python_setup import generate_distutils_setup
+from distutils import util
 
+PKG_NAME = "rwa3_group"
+package_path = util.convert_path('src/' + PKG_NAME)
+# robots_path = util.convert_path(package_path + '/robots')
 
-setup_args = generate_distutils_setup(
-    packages=['rwa3_group'],
-    package_dir={'': 'src'}
+setup(
+    version="0.0.0",
+     package_dir = {
+        PKG_NAME: package_path},
+     packages=[PKG_NAME]
+    # package_dir = {
+    #     PKG_NAME: package_path,
+    #     'robots': robots_path,
+    #     'scheduler': scheduler_path,
+    #     'tasks': tasks_path,
+    #     'utils': utils_path},
+    # packages=[PKG_NAME, 'robots', 'scheduler', 'tasks', 'utils'],
+    # install_requires=['numpy']
 )
-
-setup(**setup_args)
